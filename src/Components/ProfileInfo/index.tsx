c9083cc20f9304c14bb1ca96a4fdf2d72d11406a
vishwa-akshat <akvs0707@gmail.com>
2023-07-14 10:59:11 +0530

    Added poppins font and Profile Info component for navbar

--- src/Components/ProfileInfo/index.tsx
+++ src/Components/ProfileInfo/index.tsx
@@ -1,15 +1,32 @@
+"use client";
+
 import React from "react";
+import { Typography } from "antd";
 
 import Avatar from "@src/Components/Avatar";
 
+import ArrowDown from "@src/assets/Arrow-down.svg";
+
 import "./profileInfo.scss";
+import Image from "next/image";
 
-type Props = {};
+type Props = {
+    name?: string;
+};
 
-export default function ProfileInfo({}: Props) {
+const { Text } = Typography;
+
+export default function ProfileInfo({ name = "Xanthe Neal" }: Props) {
     return (
         <div className="profile-info-wrapper">
             <Avatar />
+            <Text className="name">{name}</Text>
+            <Image
+                className="arrow-down-icon"
+                src={ArrowDown}
+                alt="arrow down"
+                width={25}
+            />
         </div>
     );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import "./globals.scss";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
 import Navbar from "@src/Components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export const metadata = {
     title: "Thullo App",
@@ -17,7 +20,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <body className={poppins.className}>
                 <Navbar />
                 {children}
             </body>
