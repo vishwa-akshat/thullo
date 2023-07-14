2638dc60825be74e9874eeb8e080f20912a2a9e3
vishwa-akshat <akvs0707@gmail.com>
2023-07-14 10:25:30 +0530

    Added Sass and navbar logo added

--- src/Components/Navbar/index.tsx
+++ src/Components/Navbar/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
-import "./style.scss";
+import Logo from "@src/assets/Logo.svg";
+
+import "./navbar.scss";
 
 type Props = {};
 
 export default function Navbar({}: Props) {
-    return <div>Navbar</div>;
+    return (
+        <div className="navbar">
+            <div className="logo-wrapper">
+                <Image src={Logo} alt="thullo logo" />
+            </div>
+        </div>
+    );
 }
