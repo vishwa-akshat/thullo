e484b751c8415c5a8bbf18312000f04cf4c0b3bd
vishwa-akshat <akvs0707@gmail.com>
2023-07-28 13:35:01 +0530

    Added board adding modal

--- src/Components/BoardAddModal/index.tsx
+++ src/Components/BoardAddModal/index.tsx
@@ -1,8 +1,15 @@
 "use client";
 
-import { Modal } from "antd";
+import Image from "next/image";
+import { Modal, Input } from "antd";
+
+import GhostButton from "@src/Components/GhostButton";
 
 import useBoardAddModalStore from "@src/store/boardAddModalState";
+
+import addIcon from "@src/assets/add.svg";
+import lockIcon from "@src/assets/lock.svg";
+import imageIcon from "@src/assets/image.svg";
 
 import "./boardAddModal.scss";
 
@@ -17,14 +24,34 @@ export default function BoardAddModal({}: Props) {
 
     return (
         <Modal
-            title="Basic Modal"
+            className="board-add-modal"
             open={isBoardAddModalOpen}
             onOk={boardAddModalHandleOk}
+            okText={`Create`}
+            okButtonProps={{
+                icon: <Image width={17} height={17} src={addIcon} alt="icon" />,
+            }}
             onCancel={boardAddModalHandleCancel}
         >
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+            <Image
+                src="https://images.unsplash.com/photo-1690040849147-cb9810b8ca3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                alt="cover"
+                width={260}
+                height={80}
+                className="cover-image"
+            />
+            <Input
+                className="board-input-title"
+                placeholder="Add board title"
+            />
+            <div className="btn-wrapper">
+                <GhostButton icon={imageIcon} onClickHandler={() => null}>
+                    Cover
+                </GhostButton>
+                <GhostButton icon={lockIcon} onClickHandler={() => null}>
+                    Private
+                </GhostButton>
+            </div>
         </Modal>
     );
 }
---
+++ src/Components/GhostButton/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import { SVGProps } from "react";
+import { Button as AntButton } from "antd";
+
+import "./ghostButton.scss";
+
+type Props = {
+    children: string;
+    icon?: SVGProps<SVGElement>;
+    onClickHandler: () => void;
+};
+
+export default function GhostButton({ children, icon, onClickHandler }: Props) {
+    const handleBtnClick = () => {
+        onClickHandler();
+    };
+
+    if (icon) {
+        return (
+            <AntButton
+                icon={<Image width={15} height={15} src={icon} alt="icon" />}
+                className="ghost-button"
+                type="primary"
+                onClick={handleBtnClick}
+            >
+                {children}
+            </AntButton>
+        );
+    }
+
+    return (
+        <AntButton className="button" type="primary">
+            {children}
+        </AntButton>
+    );
+}
