04bbc710d85f67dbbf24ba86d038bee2498a0df2
vishwa-akshat <akvs0707@gmail.com>
2023-09-06 22:24:44 +0530

    Added profile page UI

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import "./profile.scss";
 import OutlinedButton from "@src/Components/OutlinedButton";
 import { Button, Divider } from "antd";
+import Image from "next/image";
 
 type Props = {};
 
@@ -23,6 +24,38 @@ export default function Profile({}: Props) {
                         <p>Some info may be visible to other people</p>
                     </div>
                     <Button className="edit-btn">Edit</Button>
+                </div>
+                <Divider className="profile-info-divider" />
+                <div className="data-block pd-sm">
+                    <div className="data-name">Photo</div>
+                    <div className="data-value">
+                        <Image
+                            src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80"
+                            width={72}
+                            height={72}
+                            alt="avatar"
+                        />
+                    </div>
+                </div>
+                <Divider className="profile-info-divider" />
+                <div className="data-block">
+                    <div className="data-name">Name</div>
+                    <div className="data-value">Xanthe Neal</div>
+                </div>
+                <Divider className="profile-info-divider" />
+                <div className="data-block">
+                    <div className="data-name">Bio</div>
+                    <div className="data-value">I am a software developer</div>
+                </div>
+                <Divider className="profile-info-divider" />
+                <div className="data-block">
+                    <div className="data-name">Phone</div>
+                    <div className="data-value">90854654564</div>
+                </div>
+                <Divider className="profile-info-divider" />
+                <div className="data-block">
+                    <div className="data-name">Email</div>
+                    <div className="data-value">xanthe.neal@gmail.com</div>
                 </div>
                 <Divider className="profile-info-divider" />
             </div>
